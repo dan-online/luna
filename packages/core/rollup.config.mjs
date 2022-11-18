1341add52a9df259fc267b35335487c539c37fba
DanCodes <dan@dancodes.online>
2022-11-18 13:19:02 +0000

    fix(deps): set rollup to version 2

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -1,18 +1,17 @@
 import { resolve } from 'path';
 import cleaner from 'rollup-plugin-cleaner';
 import typescript from 'rollup-plugin-typescript2';
-import versionInjector from 'rollup-plugin-version-injector';
 
 export default {
   input: 'src/index.ts',
   output: [
     {
       file: './dist/index.js',
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
+      format: 'cjs'
+      // exports: 'named'
+      // sourcemap: true
     }
   ],
-  external: ['type-graphql', 'env-schema', '@apollo/server', '@as-integrations/fastify', 'reflect-metadata', 'fastify'],
-  plugins: [cleaner({ targets: ['./dist'] }), typescript({ tsconfig: resolve(process.cwd(), 'src', 'tsconfig.json') }), versionInjector()]
+  external: ['type-graphql', 'env-schema', '@apollo/server', 'reflect-metadata', '@as-integrations/fastify', 'fastify'],
+  plugins: [cleaner({ targets: ['./dist'] }), typescript({ tsconfig: resolve(process.cwd(), 'src', 'tsconfig.json') })]
 };
