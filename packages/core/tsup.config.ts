4ab873be040c3e1ae167d5094798c5bfca287ef9
DanCodes <dan@dancodes.online>
2022-11-22 23:09:48 +0000

    chore(deps): move to tsup (#6)
    * chore(deps): move to tsup
    
    * chore(yarn): regen lockfile

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'path';
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  external: [
+    'type-graphql',
+    'env-schema',
+    '@apollo/server',
+    '@apollo/server/errors',
+    'reflect-metadata',
+    '@as-integrations/fastify',
+    'fastify',
+    '@typegoose/typegoose',
+    'class-validator',
+    'jsonwebtoken',
+    'mongoose',
+    'bcrypt',
+    'graphql',
+    'graphql-rate-limit',
+    'ioredis',
+    'pino'
+  ],
+  format: 'cjs',
+  tsconfig: resolve(process.cwd(), 'src', 'tsconfig.json')
+});
