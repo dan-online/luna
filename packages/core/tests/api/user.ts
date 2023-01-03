b8b76ce86e442f40e920df75324e14e092ca7ff1
DanCodes <dan@dancodes.online>
2023-01-03 11:22:00 +0100

    test: full coverage of user

---
+++ packages/core/tests/api/user.ts
@@ -0,0 +1,52 @@
+export const RegisterUser = {
+	query: `mutation Register($user: RegisterInput!) {
+  register(user: $user) {
+    token
+  }
+}`,
+	variables: {
+		user: {
+			birthday: "2000-01-01T00:00:00.000Z",
+			email: "test@dancodes.online",
+			firstName: "Dan",
+			password: "Testing@!123",
+			username: "DanTests",
+			lastName: "Codes",
+			middleName: "Middle", // Mmmm Dan Middle Codes
+			captcha: null,
+		},
+	},
+};
+
+export const LoginUser = {
+	query: `mutation Login($user: LoginInput!) {
+  login(user: $user) {
+    token
+  }
+}`,
+	variables: {
+		user: {
+			email: "test@dancodes.online",
+			password: "Testing@!123",
+		},
+	},
+};
+
+export const DeleteUser = {
+	query: `mutation Mutation {
+  deleteAccount
+}`,
+};
+
+export const User = {
+	query: `query Me {
+  me {
+    _id
+    firstName
+    lastLogin
+    lastName
+    middleName
+    username
+  }
+}`,
+};
--- packages/core/tests/resolvers/user.test.ts
+++ packages/core/tests/resolvers/user.test.ts
@@ -1,141 +1,186 @@
-import { FastifyInstance } from 'fastify';
-import { setupTests } from '../setupTests';
+import { FastifyInstance } from "fastify";
+import { RegisterUser, DeleteUser, LoginUser, User } from "../api/user";
+import { setupTests } from "../setupTests";
 
-const RegisterUser = {
-  query: `mutation Register($user: RegisterInput!) {
-  register(user: $user) {
-    token
-  }
-}`,
-  variables: {
-    user: {
-      birthday: '2000-01-01T00:00:00.000Z',
-      email: 'test@dancodes.online',
-      firstName: 'Dan',
-      password: 'Testing@!123',
-      username: 'DanTests',
-      lastName: 'Codes',
-      middleName: 'Middle', // Mmmm Dan Middle Codes
-      captcha: null
-    }
-  }
-};
+describe("Luna", () => {
+	let app: FastifyInstance;
+	let userToken: string;
 
-const LoginUser = {
-  query: `mutation Login($user: LoginInput!) {
-  login(user: $user) {
-    token
-  }
-}`,
-  variables: {
-    user: {
-      email: 'test@dancodes.online',
-      password: 'Testing@!123'
-    }
-  }
-};
+	setupTests((generatedApp) => {
+		app = generatedApp;
+	});
 
-const DeleteUser = {
-  query: `mutation Mutation {
-  deleteAccount
-}`
-};
+	test("GIVEN invalid user data THEN does not create user", async () => {
+		const req = await app.inject({
+			url: "/graphql",
+			method: "POST",
+			payload: { ...RegisterUser, variables: { user: { ...RegisterUser.variables.user, email: "test" } } },
+		});
 
-describe('Luna', () => {
-  let app: FastifyInstance;
-  let userToken: string;
+		const json = req.json();
 
-  setupTests((generatedApp) => {
-    app = generatedApp;
-  });
+		expect(json).toMatchObject({
+			errors: [
+				{
+					message: "Validation Error",
+					extensions: {
+						code: "BAD_USER_INPUT",
+						validationErrors: [
+							{
+								property: "email",
+								value: "test",
+								constraints: {
+									isEmail: "email must be an email",
+								},
+							},
+						],
+					},
+				},
+			],
+			data: null,
+		});
+	});
 
-  test('GIVEN invalid user data THEN does not create user', async () => {
-    const req = await app.inject({
-      url: '/graphql',
-      method: 'POST',
-      payload: { ...RegisterUser, variables: { user: { ...RegisterUser.variables.user, email: 'test' } } }
-    });
+	test("GIVEN no user THEN creates user", async () => {
+		const req = await app.inject({
+			url: "/graphql",
+			method: "POST",
+			payload: RegisterUser,
+		});
 
-    const json = req.json();
+		const json = req.json();
 
-    expect(json).toMatchObject({
-      errors: [
-        {
-          message: 'Validation Error',
-          extensions: {
-            code: 'BAD_USER_INPUT',
-            validationErrors: [
-              {
-                property: 'email',
-                value: 'test',
-                constraints: {
-                  isEmail: 'email must be an email'
-                }
-              }
-            ]
-          }
-        }
-      ],
-      data: null
-    });
-  });
+		expect(json).toMatchObject({
+			data: {
+				register: {
+					token: expect.any(String),
+				},
+			},
+		});
 
-  test('GIVEN no user THEN creates user', async () => {
-    const req = await app.inject({
-      url: '/graphql',
-      method: 'POST',
-      payload: RegisterUser
-    });
+		userToken = json.data.register.token;
+	});
 
-    const json = req.json();
+	test("GIVEN user w/o email THEN reject login", async () => {
+		const req = await app.inject({
+			url: "/graphql",
+			method: "POST",
+			payload: {
+				query: LoginUser.query,
+				variables: {
+					user: {
+						...LoginUser.variables.user,
+						email: "test@test.com",
+					},
+				},
+			},
+		});
 
-    expect(json).toMatchObject({
-      data: {
-        register: {
-          token: expect.any(String)
-        }
-      }
-    });
+		const json = req.json();
 
-    userToken = json.data.register.token;
-  });
+		expect(json).toMatchObject({
+			errors: [
+				{
+					message: "Invalid credentials",
+					extensions: {
+						code: "FORBIDDEN",
+					},
+				},
+			],
+		});
+	});
 
-  test('GIVEN user THEN login user', async () => {
-    const req = await app.inject({
-      url: '/graphql',
-      method: 'POST',
-      payload: LoginUser
-    });
+	test("GIVEN user w/o password THEN reject login", async () => {
+		const req = await app.inject({
+			url: "/graphql",
+			method: "POST",
+			payload: {
+				query: LoginUser.query,
+				variables: {
+					user: {
+						...LoginUser.variables.user,
+						password: "123",
+					},
+				},
+			},
+		});
 
-    const json = req.json();
+		const json = req.json();
 
-    expect(json).toMatchObject({
-      data: {
-        login: {
-          token: expect.any(String)
-        }
-      }
-    });
+		expect(json).toMatchObject({
+			errors: [
+				{
+					message: "Invalid credentials",
+					extensions: {
+						code: "FORBIDDEN",
+					},
+				},
+			],
+		});
+	});
 
-    userToken = json.data.login.token;
-  });
+	test("GIVEN user THEN login user", async () => {
+		const req = await app.inject({
+			url: "/graphql",
+			method: "POST",
+			payload: LoginUser,
+		});
 
-  test('GIVEN user THEN deletes user', async () => {
-    const req = await app.inject({
-      url: '/graphql',
-      method: 'POST',
-      payload: DeleteUser,
-      headers: {
-        authorization: `${userToken}`
-      }
-    });
+		const json = req.json();
 
-    const json = req.json();
+		expect(json).toMatchObject({
+			data: {
+				login: {
+					token: expect.any(String),
+				},
+			},
+		});
 
-    expect(json).toEqual({
-      data: {
-        deleteAccount: true
-      }
-    });
-  });
+		userToken = json.data.login.token;
+	});
+
+	test("GIVEN user THEN fetch information", async () => {
+		const req = await app.inject({
+			url: "/graphql",
+			method: "POST",
+			payload: User,
+			headers: {
+				authorization: `${userToken}`,
+			},
+		});
+
+		const json = req.json();
+		const user = RegisterUser.variables.user;
+
+		expect(json).toMatchObject({
+			data: {
+				me: {
+					_id: expect.any(String),
+					firstName: user.firstName,
+					lastName: user.lastName,
+					middleName: user.middleName,
+					username: user.username,
+				},
+			},
+		});
+	});
+
+	test("GIVEN user THEN deletes user", async () => {
+		const req = await app.inject({
+			url: "/graphql",
+			method: "POST",
+			payload: DeleteUser,
+			headers: {
+				authorization: `${userToken}`,
+			},
+		});
+
+		const json = req.json();
+
+		expect(json).toEqual({
+			data: {
+				deleteAccount: true,
+			},
+		});
+	});
 });
